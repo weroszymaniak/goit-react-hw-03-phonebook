e747d9c1f018913be186e650e8dfbc21ee75df40
weroszymaniak <weronika.szymaniak@hotmail.com>
2023-06-18 18:17:56 +0200

    added previon version of hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,71 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = contact => {
+    const isInContacts = this.state.contacts.some(
+      ({ name }) => name.toLowerCase() === contact.name.toLowerCase()
+    );
+
+    if (isInContacts) {
+      alert(`${contact.name} is already in contacts`);
+      return;
+    }
+    this.setState(prevState => ({
+      contacts: [{ id: nanoid(), ...contact }, ...prevState.contacts],
+    }));
+  };
+  removeContact = contact => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(({ id }) => id !== contact),
+      };
+    });
+    console.log('removin');
+  };
+  filterContact = e => {
+    this.setState({ filter: e.target.value });
+  };
+  showContacts = () => {
+    const { filter, contacts } = this.state;
+    const unitedContact = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(unitedContact)
+    );
+  };
+  render() {
+    const { filter } = this.state;
+    const searchedContacts = this.showContacts();
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact}></ContactForm>
+        <h2>Contacts</h2>
+        {this.state.contacts.length > 0 ? (
+          <Filter value={filter} onFilter={this.filterContact}></Filter>
+        ) : (
+          <div>Your phonebook is empty. Add first contact!</div>
+        )}
+        <ContactList
+          contacts={searchedContacts}
+          removeContact={this.removeContact}
+        ></ContactList>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import css from './ContactForm.module.css';
+const INITIAL_STATE = {
+  name: '',
+  number: '',
+};
+
+class ContactForm extends Component {
+  state = { ...INITIAL_STATE };
+
+  nameId = nanoid();
+  numberId = nanoid();
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit({ name: this.state.name, number: this.state.number });
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ ...INITIAL_STATE });
+  };
+
+  render() {
+    const { name, number } = this.state;
+    return (
+      <form className={css.form} onSubmit={this.handleSubmit}>
+        <label className={css.label}>
+          Name
+          <input
+            className={css.input}
+            type="text"
+            name="name"
+            value={name}
+            onChange={this.handleChange}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+
+        <label className={css.label}>
+          Number
+          <input
+            className={css.input}
+            type="tel"
+            name="number"
+            value={number}
+            onChange={this.handleChange}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+
+        <button className={css.button} type="submit">
+          Add contact{' '}
+        </button>
+      </form>
+    );
+  }
+}
+
+export default ContactForm;
